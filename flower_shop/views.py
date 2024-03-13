63c880bdf422740f0ed7252ad4e10d26f3d5b714
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-03-13 17:44:48 +0300

    Connect card.html to DB

--- flower_shop/views.py
+++ flower_shop/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from flower_shop.models import Bouquet
+
+
+def view_bouquet(request, bouquet_id):
+    bouquet = get_object_or_404(Bouquet, pk=bouquet_id)
+
+    return render(request, 'flower_shop/card.html', context={'bouquet': bouquet})
