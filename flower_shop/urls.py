39f17f1c17803c27513aebb71007a6bdd229166a
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-03-13 20:08:03 +0300

    Connect index.html to DB

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/', views.show_catalog, name='catalog'),
     path('bouquet/<int:bouquet_id>/', views.view_bouquet, name='bouquet_detail'),
 ]
--- flower_shop/views.py
+++ flower_shop/views.py
@@ -4,9 +4,14 @@ from flower_shop.models import Bouquet
 
 def view_bouquet(request, bouquet_id):
     bouquet = get_object_or_404(Bouquet, pk=bouquet_id)
-
     return render(request, 'flower_shop/card.html', context={'bouquet': bouquet})
 
+
 def index(request):
-    recommended_bouquets = Bouquet.objects.filter(recommended=True)[:3]
+    recommended_bouquets = Bouquet.objects.filter(is_recommended=True)[:3]
     return render(request, 'flower_shop/index.html', context={'recommended_bouquets': recommended_bouquets})
+
+
+def show_catalog(request):
+    bouquets = Bouquet.objects.all()
+    return render(request, 'flower_shop/catalog.html', context={'bouquets': bouquets})
