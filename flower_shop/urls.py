28a8e75393a4f66f265fefa28197864b205cfd10
romanulanov <turnsourcream@gmail.com>
2024-03-14 14:03:01 +0400

    добавил карту оплаты

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('catalog/', views.show_catalog, name='catalog'),
     path('bouquet/<int:bouquet_id>/', views.view_bouquet, name='bouquet_detail'),
+    path('checkout/', views.checkout, name='catalog'),
+    path('charge/', views.charge, name='charge'),
 ]
