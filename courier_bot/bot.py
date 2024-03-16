24386de2b8640f31b5e480b5391ccb8ddee9b233
romanulanov <turnsourcream@gmail.com>
2024-03-16 18:45:36 +0400

    изменил отправку сообщений курьерам

--- courier_bot/bot.py
+++ courier_bot/bot.py
@@ -1,9 +1,8 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 import telebot
 from django.conf import settings
+from flower_shop.models import Courier
 
-courier_id = settings.COURIER_TG_ID 
+courier_id = settings.COURIER_TG_ID
 telegrambot_token = settings.TELEGRAMBOT_TOKEN
 
 bot = telebot.TeleBot(telegrambot_token)
@@ -11,11 +10,18 @@ bot = telebot.TeleBot(telegrambot_token)
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    bot.send_message(courier_id, "В этот чат будут приходить новые заказы")
+    bot.send_message(message.from_user.id,
+                     "В этот чат будут приходить новые заказы")
 
 
 def send_order(instance):
-    bot.send_message(courier_id, f"Новый заказ:\nФИО: {instance.contact_name}\nТелефон: {instance.contact_phone}\nБукет: {instance.bouquet.name}\nТип оплаты: {instance.payment_type}")
+    couriers = Courier.objects.all()
+    for courier in couriers:
+        bot.send_message(courier.tg_id, f'''Новый заказ:
+                        ФИО: {instance.contact_name}
+                        Телефон: {instance.contact_phone}
+                        Букет: {instance.bouquet.name}
+                        Тип оплаты: {instance.payment_type}''')
 
 
 def run_courier_bot():
