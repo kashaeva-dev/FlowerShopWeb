579f05a1206c2e2fe6d5fc92c6693e4c9b64d267
romanulanov <turnsourcream@gmail.com>
2024-03-17 20:00:14 +0400

    фикс отправки курьеру

--- courier_bot/bot.py
+++ courier_bot/bot.py
@@ -1,6 +1,6 @@
 import telebot
 from django.conf import settings
-from flower_shop.models import Courier
+from account.models import Staff
 
 courier_id = settings.COURIER_TG_ID
 telegrambot_token = settings.TELEGRAMBOT_TOKEN
@@ -14,14 +14,15 @@ def start_handler(message):
                      "В этот чат будут приходить новые заказы")
 
 
-def send_order(instance):
-    couriers = Courier.objects.all()
-    for courier in couriers:
-        bot.send_message(courier.tg_id, f'''Новый заказ:
-                        ФИО: {instance.contact_name}
-                        Телефон: {instance.contact_phone}
-                        Букет: {instance.bouquet.name}
-                        Тип оплаты: {instance.payment_type}''')
+def send_order(order):
+    curriers = Staff.objects.filter(role='currier')
+    for currier in curriers:
+        if order.currier == currier:
+            bot.send_message(currier.tg_id, f'''Новый заказ:
+                            ФИО: {order.contact_name}
+                            Телефон: {order.contact_phone}
+                            Букет: {order.bouquet.name}
+                            Тип оплаты: {order.payment_type}''')
 
 
 def run_courier_bot():
--- flower_shop/management/commands/run_bot.py
+++ flower_shop/management/commands/run_bot.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
-from courier_bot.bot import run_courier_bot  
+from courier_bot.bot import run_courier_bot
+
 
 class Command(BaseCommand):
     help = 'Запуск бота'
 
     def handle(self, *args, **options):
-        run_courier_bot() 
+        run_courier_bot()
